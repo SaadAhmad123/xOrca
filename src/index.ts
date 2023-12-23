8acff4f2665db6995e7fe5e23d0ca267a6470fdb
Saad Ahmad <saadkwi12@hotmail.com>
2023-12-24 00:08:33 +1100

    update

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
-import { StorageManagerWithLocking } from './storage_manager';
+import { StorageManagerWithLocking, LockingManager } from './storage_manager';
 import LocalFileStorageManager from './storage_manager/local_file_storage_manager';
 import S3StorageManager from './storage_manager/s3_storage_manager';
 import PersistedActor, { withPersistedActor } from './persisted_actor';
 import { PersistanceLockError } from './utils';
 
 export {
+  LockingManager,
   StorageManagerWithLocking,
   LocalFileStorageManager,
   S3StorageManager,
